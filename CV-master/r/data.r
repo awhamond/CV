dd818ae42f071ec7d917b529705036edaf0d7f04
awhamond <136225712+awhamond@users.noreply.github.com>
2024-01-12 19:09:11 +1100

    Initial commit

---
+++ CV-master/r/data.r
@@ -0,0 +1,44 @@
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Academic Tutor", "Psychological Sciences", "February", 2023, "Present", NA, "University of Melbourne", "Foster student engagement during structured tutorials",
+  "Academic Tutor", "Psychological Sciences", "February", 2023, "Present", NA, "University of Melbourne", "Respond to students' questions and concerns through timely and professional email communication",
+  "Academic Tutor", "Psychological Sciences", "February", 2023, "Present", NA, "University of Melbourne", "Meet strict deadlines marking assessments and essays",
+  "Dance and Fitness Instructor", "Freelance", "March", 2012, "November", 2021, "Vancouver | Berlin | Melbourne", "Created multi-level, progressive physical training and dance choreography classes",
+  "Dance and Fitness Instructor", "Freelance", "March", 2012, "November", 2021, "Vancouver | Berlin | Melbourne", "Conveyed basic principles of exercise physiology and biomechanics to support student ecperience",
+  "Dance and Fitness Instructor", "Freelance", "March", 2012, "November", 2021, "Vancouver | Berlin | Melbourne", "Competed and performed dance internationally",
+)
+
+skills <- tribble(
+  ~head, ~detail,
+  "General Competencies", "RStudio: ggplot data visualisation, multivariate statistical analyses, reproducible research documentation",
+  "General Competencies", "Language proficiencies: intermediate German, Spanish, Middle Egyptian, and basic Latin.",
+  "General Competencies", "Confident in public speaking and leadership roles.",
+  "General Competencies", "Experienced welcoming diversity and encouraging inclusivity in classroom environments.",
+  "General Competencies", "Comfortable working independently, as a team, or supervised.",
+  "General Competencies", "Consistently punctual, professional, and reliable."
+)
+
+certifications <- tribble(
+    ~cert, ~startYear, ~endYear, ~inst, ~where, ~detail,
+    "The TEFL Academy", 2022, 2022, "Teaching English as a Foreign Language", "Online", "Level 5 Diploma", 
+    "Australian Institute of Fitness", 2013, 2014, "Certificate IV in Fitness", "Melbourne, Australia", "Group Fitness and Personal Trainer Qualification",
+)
+
+
+awards <- tribble(
+    ~area, ~accomplishment, ~year, ~where,
+    "Dean's Honour List", "Granted to individuals in the top 3% of their bachelor program (1st year)", 2020, "University of Melbourne",
+    "Dean's Honour List", "Granted to individuals in the top 3% of their bachelor program (2nd year)", 2021, "University of Melbourne",
+)
+
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+    "Psychology Major", 2020, 2023, "Bachelor of Arts (First Class Honours)", "University of Melbourne", "4th year thesis supervisors: Prof. Simine Vazire & Dr. Tom Hardwicke",
+)
+
+
+# INCOMPLETE CODE
+references <- tribble(
+  ~detail,
+  
+)
